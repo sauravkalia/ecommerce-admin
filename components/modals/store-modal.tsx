894937455fabc2bf3204a555d6207a8130806f21
Saurav Kalia <sauravhunny@gmail.com>
2023-07-22 13:17:05 +0530

    from and input package added in chadcn

--- components/modals/store-modal.tsx
+++ components/modals/store-modal.tsx
@@ -1,10 +1,33 @@
 "use client";
 
+import * as z from "zod";
+
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod"
+
 import { useStoreModal } from "@/hooks/use-store-modal";
 import { Modal } from "@/components/ui/modal";
+import { Form } from "@/components/ui/form";
+
+const formSchema = z.object({
+  name: z.string().min(1),
+});
 
 export const StoreModal = () => {
   const storeModal = useStoreModal();
+
+  const form  = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: "",
+    },
+  })
+
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    console.log(values);
+    // TODO: Create Store
+    
+  }
 
   return (
     <Modal
@@ -12,7 +35,15 @@ export const StoreModal = () => {
     description="Add a new store to manage products and categories"
     isOpen={storeModal.isOpen}
     onClose={storeModal.onClose}>
-      Future Create Store Form
+      <div>
+        <div className="space-y-4 py-2 pb-4">
+          <Form
+          {...form}
+          >
+            
+          </Form>
+        </div>
+      </div>
     </Modal>
   )
 }
