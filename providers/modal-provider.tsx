d84298e40e01a80107ac9c25f7df0035cd82859c
Saurav Kalia <sauravhunny@gmail.com>
2023-07-22 13:01:25 +0530

    useStoreModal ued in root page.tsx

--- providers/modal-provider.tsx
+++ providers/modal-provider.tsx
@@ -2,3 +2,24 @@
 
 import { useEffect, useState } from "react";
 
+import { StoreModal } from "@/components/modals/store-modal";
+
+export const ModalProvider = () => {
+  const[isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, [])
+
+  if(!isMounted) {
+    return null;
+  }
+
+
+  return (
+    <>
+    <StoreModal />
+    </>
+  )
+}
+
