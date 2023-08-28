f0c12e34f088d581b99a757411ffc4451db9650a
Saurav Kalia <sauravhunny@gmail.com>
2023-08-28 20:17:56 +0530

    redirection added once created a modal

--- components/modals/store-modal.tsx
+++ components/modals/store-modal.tsx
@@ -37,7 +37,7 @@ export const StoreModal = () => {
 
       const response = await axios.post('/api/stores', values);
 
-      toast.success('Store created');
+      window.location.assign(`/${response.data.id}`)
       
     } catch (error) {
       console.log(error);
